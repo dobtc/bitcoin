82e88fec2bd2880b0dc406e534cd63d8f13943dd
Kroese <kroese@users.noreply.github.com>
2026-07-04 02:07:01 +0200

    fix: Entrypoint argument handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,26 @@ if [ -n "${GID+x}" ] && [ "${GID}" != "0" ]; then
   groupmod -g "$GID" bitcoin
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ]; then
-  set -- bitcoind "$@"
-fi
+case "${1:-}" in
+  -*)
+    set -- bitcoind "$@"
+    ;;
+esac
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
+if [ "${1:-}" = "bitcoind" ]; then
   mkdir -p "$BITCOIN_DATA"
   chmod 700 "$BITCOIN_DATA"
+
   # Fix permissions for home dir.
   chown -R bitcoin:bitcoin "$(getent passwd bitcoin | cut -d: -f6)"
+
   # Fix permissions for bitcoin data dir.
   chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
 
-if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
+if [ "${1:-}" = "bitcoind" ] || [ "${1:-}" = "bitcoin-cli" ] || [ "${1:-}" = "bitcoin-tx" ]; then
   echo
   exec gosu bitcoin "$@"
 fi
